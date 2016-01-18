2dd7d5f02d9263e30022fbbb84f2906d9643f542
karunru <1qazhikaru@gmail.com>
2016-01-18 11:37:12 +0900

    usernameでログインできるようにした:grin:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117130148) do
+ActiveRecord::Schema.define(version: 20160118022138) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer  "schedule_type"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160117130148) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
