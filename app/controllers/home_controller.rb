d9e2d5510bdcf75a666356f70eea95c15b2f27ae
karunru_sakura <1qazhikaru@gmail.com>
2016-03-03 19:53:19 +0900

    add sort:herb:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
 		@summonses = Summons.all
-		@schedules = Schedule.all
+		@schedules = Schedule.all.order(:deadline_date)
  	end
 end
--- app/controllers/summonses_controller.rb
+++ app/controllers/summonses_controller.rb
@@ -1,5 +1,6 @@
 class SummonsesController < ApplicationController
   before_action :set_summons, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except=>[:home]
 
   # GET /summonses
   # GET /summonses.json
