671b7c1f18598add7acc3ecfa3277843da392d83
karunru <1qazhikaru@gmail.com>
2016-03-08 19:08:33 +0900

    コメントを追加:pencil2:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
 		@summonses = Summons.all
-		@schedules = Schedule.all.order(:deadline_date)
+		@schedules = Schedule.all.order(:deadline_date) #order()で表示するときソートしてる
  	end
 end
