275273e18226cf08498030234bfbbd137da51050
karunru_sakura <1qazhikaru@gmail.com>
2016-07-11 18:21:25 +0900

    rails5にアプデしたらemailがuniqueじゃないって言われるようになってhttp://qiita.com/katryo/items/f04ad7635a7ca92d8688ここを参考にしてfixした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720055819) do
+ActiveRecord::Schema.define(version: 20160711091727) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer  "schedule_type"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160720055819) do
     t.string   "submit_method"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "classroom"
   end
 
   create_table "summonses", force: :cascade do |t|
@@ -31,7 +30,6 @@ ActiveRecord::Schema.define(version: 20160720055819) do
     t.string   "place"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "classroom"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,8 +46,6 @@ ActiveRecord::Schema.define(version: 20160720055819) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.string   "role"
-    t.string   "classroom"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
