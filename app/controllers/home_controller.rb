24114d56acdb8bab8db3e3ab16da832c48544be1
karunru_sakura <1qazhikaru@gmail.com>
2016-07-11 18:21:25 +0900

    rails5にアプデしたらemailがuniqueじゃないって言われるようになってhttp://qiita.com/katryo/items/f04ad7635a7ca92d8688ここを参考にしてfixした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+before_filter :authenticate_user!, :except=>[:home]
   def index
 		@summonses = Summons.all
 		@schedules = Schedule.all.order(:deadline_date) #order()で表示するときソートしてる
