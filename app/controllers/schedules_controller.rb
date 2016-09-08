6c733aff362eee1005273b512f4c39bc832ae754
karunru <1qazhikaru@gmail.com>
2016-09-08 17:38:52 +0900

    memberでも/schedulesと/summonses直接叩けば行けちゃったからapplication_controllerにredirect_unless_adminを移して各コントローラで使いまわした

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,7 @@
 class SchedulesController < ApplicationController
   before_action :set_schedule, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, :except=>[:home]
+	before_filter :authenticate_user!, :redirect_unless_admin
 
   # GET /schedules
   # GET /schedules.json
