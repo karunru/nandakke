ebf75fa79aa379f064b7d9f5a26f6595a0a73ef1
karunru <1qazhikaru@gmail.com>
2016-10-11 13:33:20 +0900

    csvでseedできるようにした #9

---
+++ db/make_data.jl
@@ -0,0 +1,10 @@
+for i in 1:5
+	println("J", i, "-teacher", ",", "editor", ",", "J", i)
+	println("J", i, "-pi", ",", "member", ",", "J", i)
+	for j in 01:50
+		println("j", 17 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
+	end
+	for j in 01:50
+		println("pj", 17 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
+	end
+end
