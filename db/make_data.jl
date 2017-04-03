e81efbe9c5d4644ccaed5d5c94333f1cc8b1a5b6
karunru <1qazhikaru@gmail.com>
2017-04-03 10:52:45 +0900

    2017年度用に変更

--- db/make_data.jl
+++ db/make_data.jl
@@ -2,9 +2,9 @@ for i in 1:5
 	println("J", i, "-teacher", ",", "editor", ",", "J", i)
 	println("J", i, "-pi", ",", "member", ",", "J", i)
 	for j in 01:50
-		println("j", 17 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
+		println("j", 18 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
 	end
 	for j in 01:50
-		println("pj", 17 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
+		println("pj", 18 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
 	end
 end
