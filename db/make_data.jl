646bc763d90a42d56252bde7518a2fe828f827cd
karunru <1qazhikaru@gmail.com>
2017-04-04 21:15:50 +0900

    権限変更に伴った修正

--- db/make_data.jl
+++ db/make_data.jl
@@ -1,6 +1,6 @@
 for i in 1:5
-	println("J", i, "-teacher", ",", "editor", ",", "J", i)
-	println("J", i, "-pi", ",", "member", ",", "J", i)
+	println("J", i, "-teacher", ",", "teacher", ",", "J", i)
+	println("pi-J", i, ",", "member", ",", "J", i)
 	for j in 01:50
 		println("j", 18 - i, "4", lpad(j,2,"0"), ",", "member", ",", "J", i)
 	end
