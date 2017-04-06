89db87b7f0df7607542db9dd6db61a9bc96d919c
karunru <1qazhikaru@gmail.com>
2017-04-06 12:06:52 +0000

    各ユーザが利用可能なリンクを/adminにまとめた #10

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
 #load_and_authorize_resource
 before_filter :authenticate_user!, :except=>[:home]
-before_filter :authenticate_user!, :redirect_unless_admin
+
   def index
   end
 
